4dc81c8003d8acabc9289e4dd20e1a7e6c95da5d
mizosoft <moataz.nasser20@gmail.com>
2026-05-21 01:17:58 +0200

    Remove unused method

--- benchmarks/util.go
+++ benchmarks/util.go
@@ -38,38 +38,3 @@ func newClusterClient(b *testing.B, nodeCount int, batchInterval time.Duration) 
 
 	return cluster, kvClient
 }
-
-func newClusterClientWithWalPersistence(b *testing.B, nodeCount int, batchInterval time.Duration) (*infratesting.Cluster, *client.KvClient) {
-	cluster, err := infratesting.StartLocalCluster(
-		infratesting.ClusterConfig{
-			Dir:                   b.TempDir(),
-			NodeCount:             nodeCount,
-			HeartbeatMillis:       50,
-			ElectionTimeoutMillis: graft.IntRange{Low: 150, High: 300},
-			ServerFactory: func(address string, config graft.Config) (infratesting.BaseServer, error) {
-				return service.NewKvServer(address, batchInterval, config)
-			},
-			PersistenceFactory: func(dir string) (graft.Persistence, error) {
-				return graft.OpenWal(graft.WalOptions{
-					Dir:             dir,
-					SegmentSize:     64 * 1024 * 1024,
-					SuffixCacheSize: 1 * 1024 * 1024,
-				})
-			},
-			Logger: zap.NewNop(),
-		},
-	)
-
-	if err != nil {
-		b.Fatalf("Couldn't start cluster: %v", err)
-	}
-
-	kvClient := client.NewKvClient("client-"+b.Name(), cluster.ServiceConfig())
-
-	err = kvClient.CheckHealthy()
-	if err != nil {
-		b.Fatalf("Couldn't connect to cluster: %v", err)
-	}
-
-	return cluster, kvClient
-}
